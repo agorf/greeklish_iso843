90cda40e5c50cb472a8e6875ee1ca609ea48bac4
Angelos Orfanakos <git@angelos.dev>
2021-03-04 18:01:51 +0200

    Add first tests

---
+++ test/test_converter.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'greeklish_iso843'
+
+class GreeklishIso843::ConverterTest < Minitest::Test
+  TEST_PAIRS = {
+    'Μπάμπης' => 'Bampis',
+    'Άγγελος' => 'Angelos',
+    'Ευάγγελος' => 'Evangelos',
+    'άνευ αποδοχών' => 'anef apodochon',
+    'εγγύηση' => 'engyisi',
+    'Εύβοια' => 'Evvoia',
+    "εφ' όλης της ύλης" => "ef' olis tis ylis"
+  }.freeze
+
+  TEST_PAIRS.each do |greek, greeklish|
+    name = greeklish.downcase.gsub(/\W+/, '')
+
+    define_method "test_#{name}" do
+      assert_equal GreeklishIso843::Converter.convert(greek), greeklish
+    end
+  end
+end
