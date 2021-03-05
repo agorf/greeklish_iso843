9d5ad39302559b37a420b8c7ef86de5eff0ca587
Angelos Orfanakos <git@angelos.dev>
2021-03-05 19:36:19 +0200

    Configure RuboCop and fix offenses

--- lib/greeklish_iso843/converter.rb
+++ lib/greeklish_iso843/converter.rb
@@ -79,31 +79,14 @@ class GreeklishIso843::Converter
   def convert
     text.gsub(REPLACEMENTS_REGEXP) do |match|
       match_data = Regexp.last_match
-      prev_char = match_data.pre_match[-1]&.downcase
       next_char = match_data.post_match[0]&.downcase
       greeklish = REPLACEMENTS[match.downcase]
       greek = match
 
       if greeklish
         greek = match + next_char.to_s
-      elsif match.casecmp?('μπ')
-        greeklish =
-          if prev_char && GREEK_LOWER[prev_char] && # *μπ
-              next_char && GREEK_LOWER[next_char] # and μπ*
-            'mp'
-          else # μπ* or *μπ
-            'b'
-          end
-      elsif match.casecmp?('τς')
-        greeklish = 'ts'
-      else # αυ αύ ευ εύ ηυ ηύ
-        greeklish =
-          REPLACEMENTS[match[0].downcase] +
-          if next_char && 'αάεέηήιίϊΐοόυύϋΰωώβγδζλμνρ'[next_char]
-            'v'
-          else
-            'f'
-          end
+      else
+        greeklish = convert_to_greeklish(match, match_data, next_char)
       end
 
       fix_case(greeklish, greek)
@@ -115,14 +98,42 @@ class GreeklishIso843::Converter
   end
 
   private def fix_case(greeklish, greek)
-    if !capitalized?(greek[0])
-      return greeklish
-    end
+    return greeklish if !capitalized?(greek[0])
 
     if greek.length == 1 || capitalized?(greek[1])
       greeklish.upcase
     else
       greeklish[0].upcase + greeklish[1..-1]
     end
+  end
+
+  private def convert_mp(prev_char, next_char)
+    if prev_char && GREEK_LOWER[prev_char] && # *μπ
+        next_char && GREEK_LOWER[next_char] # and μπ*
+      'mp'
+    else # μπ* or *μπ
+      'b'
+    end
+  end
+
+  private def convert_v_or_f(match, next_char)
+    REPLACEMENTS[match[0].downcase] +
+      if next_char && 'αάεέηήιίϊΐοόυύϋΰωώβγδζλμνρ'[next_char]
+        'v'
+      else
+        'f'
+      end
+  end
+
+  private def convert_to_greeklish(match, match_data, next_char)
+    return 'ts' if match.casecmp?('τς')
+
+    if match.casecmp?('μπ')
+      prev_char = match_data.pre_match[-1]&.downcase
+      return convert_mp(prev_char, next_char)
+    end
+
+    # αυ αύ ευ εύ ηυ ηύ
+    convert_v_or_f(match, next_char)
   end
 end
